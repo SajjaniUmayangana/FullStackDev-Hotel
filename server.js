200911e4436216001494aca5f1ff9574be5ea394
SajjaniUmayangana <sumayangana2428@gmail.com>
2022-12-21 23:09:44 +0530

    server

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
+var cookieParser = require('cookie-parser');
+
 
 // importing route files 
 const hotelRoute = require('./routes/hotel')
 const roomRoute = require('./routes/room')
 const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
+const authRoute = require('./routes/auth');
+
+
 
 dotenv.config() // connecting the config.env with the server.js file
 
@@ -28,11 +32,20 @@ mongoose.connection.on("disconnected", () => {
 
 // middleware 
 app.use(express.json())
+app.use(cookieParser());
+
 
 app.use("/hotel", hotelRoute);
 app.use("/room", roomRoute);
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+
+// Error handeling middleware 
+app.use((err,req,res,next)=>{
+  const errStatus = err.status || 500
+  const errMessage = err.message || "Sorry, something went wrong"
+  return res.status(errStatus).json(errMessage)
+})
 
 
 app.listen(8800, () => {
