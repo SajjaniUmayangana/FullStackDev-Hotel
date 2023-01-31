838d3a7a6809ec6d44b7ffb99a03a18cf1a025ca
SajjaniUmayangana <sumayangana2428@gmail.com>
2023-01-31 17:51:40 +0530

    server

--- server.js
+++ server.js
@@ -7,43 +7,7 @@ const server = http.createServer(app);
 const path = require("path");
 const axios = require('axios');
 var cookieParser = require('cookie-parser');
-
-const webSocket = require('ws')
-const socketio = require('socket.io')
-const io = socketio(server)
-
-// Run when client connects 
-io.on('connection', socket => {
-  console.log('New WS Connection...');
-})
- 
-
-
-/*
-// websocket
-const wss = new webSocket.Server({server});
-
-// line 18-28 gets triggered when a new connection is made 
-wss.on('connection', function connection(ws) {
-  console.log('A new client connected')
-  ws.send('Welcome new client');
-
-  //this code triggers when the server recives msg from the client 
-  ws.on('message', function incoming(message){
-    console.log('received: %s', message);
-    ws.send('got your msg its: '+ message)
-  })  
-})
-*/
-
-
-
-// importing route files 
-const hotelRoute = require('./routes/hotel')
-const roomRoute = require('./routes/room')
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth');
-const { Socket } = require("socket.io");
+const message = require("./model/message_schema");
 
 // connecting the config.env with the server.js file
 dotenv.config() 
@@ -62,6 +26,36 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB is disconnected")
 })
 
+
+// To send flash messages to the front-end
+var session = require('express-session');
+var flash = require('connect-flash');
+
+app.use(session({
+  secret: 'secret',
+  cookie: {maxAge : 3000},
+  saveUninitialized : false,
+  resave : false
+}))
+
+app.use(flash());
+
+
+// importing route files 
+const roomRoute = require('./routes/room')
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth');
+const employeeRoute = require('./routes/employee');
+
+// middleware 
+app.use(express.json())
+app.use(cookieParser());
+
+app.use("/room", roomRoute);
+app.use("/user", userRoute);
+app.use("/auth", authRoute);
+app.use("/employee", employeeRoute);
+
 // static files 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
@@ -74,13 +68,6 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
-
-// Error handeling middleware 
-app.use((err,req,res,next)=>{
-  const errStatus = err.status || 500
-  const errMessage = err.message || "Sorry, something went wrong"
-  return res.status(errStatus).json(errMessage)
-})
 
 // Rendering webpages
 app.get('', (req,res)=>{
@@ -99,34 +86,77 @@ app.get('/loginpage', (req,res)=>{
   res.render('loginpage.ejs')
 })
 
-app.get('/chatRoom', (req,res)=>{
-  res.render('chatRoom.ejs')
+app.get('/adminUsers', (req,res)=>{
+  res.render('admin.ejs', {message : req.flash('message')})
 })
 
-app.get('/chat', (req,res)=>{
-  res.render('chat.ejs')
+app.get('/adminRooms', (req,res)=>{
+  res.render('adminRooms.ejs', {message : req.flash('message')})
 })
 
-app.get('/chatRoomAdmin', (req,res)=>{
-  res.render('adminChat.ejs')
+app.get('/adminEmployee', (req,res)=>{
+  res.render('adminEmployee.ejs', {message : req.flash('message')})
 })
 
-/*
-app.get('/chat', (req,res)=>{
-  res.render('loginpage.ejs') // when "Chat with us" is clicked it needs to be taken back to the login page
+app.get('/adminHome', (req,res)=>{
+  res.render('adminHome.ejs')
 })
-*/
+
+
+app.get('/displayrooms', (req,res)=>{
+  res.render('displayRooms.ejs')
+})
+
+app.get('/booking', (req,res)=>{
+  res.render('booking.ejs')
+})
+
+app.get('/roomdetails', (req,res)=>{
+  res.render('roomdetail.ejs')
+})
+
+app.get('/reviews', (req,res)=>{
+  res.render('userReviews.ejs')
+})
 
 
 
-// middleware 
-app.use(express.json())
-app.use(cookieParser());
+// Socket connection
+const io = require("socket.io")(server); 
 
-app.use("/hotel", hotelRoute);
-app.use("/room", roomRoute);
-app.use("/user", userRoute);
-app.use("/auth", authRoute);
+// Socket handeling 
+io.on("connection", function (socket) {
+
+       message.find().then(result => {
+              socket.emit('output-message', result)
+       })
+
+      console.log("User connected", socket.id);
+
+       socket.on('disconnect', ()=> {
+               console.log('user disconnected');
+        });
+
+       socket.on('chatmessage', msg => {
+                const newmessage = new message({ msg:msg});
+
+                newmessage.save().then(()=>{
+                io.emit('message', msg)
+         })
+      })
+});
+
+
+
+
+
+// Error handeling middleware 
+app.use((err,req,res,next)=>{
+  const errStatus = err.status || 500
+  const errMessage = err.message || "Sorry, something went wrong"
+  return res.status(errStatus).json(errMessage)
+})
+
 
 server.listen(8800, () => {
   console.log("connected to backend");
